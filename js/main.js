9132a1a463da07fd5c9a91d19af70562fcabbe25
Анна <mambarinka@mail.ru>
2020-10-22 16:34:17 +0400

    feat: some little changes about names exports

--- js/main.js
+++ js/main.js
@@ -1,49 +1,52 @@
 'use strict';
 
 (() => {
-  let fieldsets = document.querySelectorAll('fieldset');
+  const fieldsets = document.querySelectorAll('fieldset');
 
   //  функция для перехода в НЕАКТИВНОЕ состояние страницы
-  let deactivatePage = () => {
+  const deactivatePage = () => {
     window.mock.map.classList.add('map--faded');
     window.form.form.classList.add('ad-form--disabled');
     window.form.addDisabledAttribute(fieldsets);
-    window.form.setDefaultAddress();
-    window.pin.hideMapPins();
+    window.pinMain.setAddress(window.pinMain.coordsDefault);
+    window.map.hideMapPins();
   };
 
-  let resetButton = document.querySelector('.ad-form__reset');
+  deactivatePage();
+
+  const resetButton = document.querySelector('.ad-form__reset');
 
   resetButton.addEventListener('click', () => {
     deactivatePage();
   });
 
   // обработчик для активации страницы основной (левой) кнопкой мыши
-  window.form.pinMain.addEventListener('mouseup', (evt) => {
+  window.pinMain.pinMain.addEventListener('mousedown', (evt) => {
     if (evt.button === 0) {
       activatePage();
     }
   });
 
   // обработчик для активации страницы с клавиатуры клавишей enter
-  window.form.pinMain.addEventListener('keydown', (evt) => {
+  window.pinMain.pinMain.addEventListener('keydown', (evt) => {
     if (evt.key === 'Enter') {
       activatePage();
     }
   });
 
   // функция для перехода в АКТИВНОЕ состояние страницы
-  let activatePage = () => {
+  const activatePage = () => {
     window.mock.map.classList.remove('map--faded');
     window.form.form.classList.remove('ad-form--disabled');
     // window.util.createArrayObjects();
-    window.mock.createArray();
+    //  window.mock.createArray();
+    window.map.getRandomCards();
     window.form.removeDisabledAttribute(fieldsets);
-    window.form.setCustomAddress();
+    window.pinMain.setAddress(window.pinMain.coordsCustom);
 
-    window.pin.showMapPins();
+    window.map.showMapPins();
 
-    window.form.pinMain.removeEventListener('mouseup', activatePage);
-    window.form.pinMain.removeEventListener('keydown', activatePage);
+    window.pinMain.pinMain.removeEventListener('mouseup', activatePage);
+    window.pinMain.pinMain.removeEventListener('keydown', activatePage);
   };
 })();
