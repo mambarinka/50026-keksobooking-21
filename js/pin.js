e600c282d8a9ff1299de6ab3942374b995e11e19
Анна <mambarinka@mail.ru>
2020-10-22 16:31:11 +0400

    fix: delete functions, its using in map module

--- js/pin.js
+++ js/pin.js
@@ -5,59 +5,59 @@
   const PIN_HEIGHT = 70;
 
   //  шаблон метки
-  let pinTemplate = document.querySelector('#pin')
+  const pinTemplate = document.querySelector('#pin')
     .content
     .querySelector('.map__pin');
 
   //  функция создания DOM-элемента на основе шаблона метки
-  let createPin = (add) => {
-    let pin = pinTemplate.cloneNode(true);
+  const createPin = (object) => {
+    const pin = pinTemplate.cloneNode(true);
 
-    pin.style.left = `${add.location.x - (PIN_WIDTH / 2)}px`;
-    pin.style.top = `${add.location.y - PIN_HEIGHT}px`;
+    pin.style.left = `${object.location.x - (PIN_WIDTH / 2)}px`;
+    pin.style.top = `${object.location.y - PIN_HEIGHT}px`;
 
-    pin.querySelector('img').src = add.author.avatar;
-    pin.querySelector('img').alt = add.offer.title;
+    pin.querySelector('img').src = object.author.avatar;
+    pin.querySelector('img').alt = object.offer.title;
 
     //  pin.addEventListener('click', openPopup(add));
 
     pin.addEventListener('click', () => {
-      window.card.openPopup(add);
+      window.card.openPopup(object);
     });
 
     return pin;
   };
 
-  let offers = window.mock.createArray();
+  // let offers = window.mock.createArray();
 
-  let makeFragment = (elements) => {
+  const makeFragment = (elements) => {
     let pinsfragment = document.createDocumentFragment();
     for (let j = 0; j < elements.length; j++) {
-      let pinElement = createPin(elements[j]);
+      const pinElement = createPin(elements[j]);
       pinsfragment.appendChild(pinElement);
     }
     return pinsfragment;
   };
 
-  let showMapPins = () => {
-    let fragmentWithObjects = makeFragment(offers);
-    window.card.pinsContainer.appendChild(fragmentWithObjects);
-  };
+  //  let showMapPins = () => {
+  //  let fragmentWithObjects = makeFragment(offers);
+  //  window.card.pinsContainer.appendChild(fragmentWithObjects);
+  // };
 
-  let hideMapPins = () => {
-    let notMainPins = window.card.pinsContainer.querySelectorAll(`.map__pin:not(.map__pin--main)`);
+  // let hideMapPins = () => {
+  // let notMainPins = window.card.pinsContainer.querySelectorAll(`.map__pin:not(.map__pin--main)`);
 
-    notMainPins.forEach((pin) => {
-      pin.remove();
-    });
-  };
+  //  notMainPins.forEach((pin) => {
+  //  pin.remove();
+  //  });
+  //  };
 
   window.pin = {
     PIN_WIDTH,
     PIN_HEIGHT,
     createPin,
-    showMapPins,
-    hideMapPins,
+    // showMapPins,
+    // hideMapPins,
     makeFragment
   };
 
